fb194d2959ef3d2798e7d82fd780473142f3269b
rahulcendrol <rahulchouhan.cendrol@gmail.com>
2026-01-04 16:48:20 +0530

    testing dev branch

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const { getUsers } = require("../controllers/user.controller");
 const { createUser } = require("../controllers/user.controller");
 
+
+// user data
 //GET
 router.get("/", getUsers);
-
 //POST
 router.post("/", createUser);
+
+//contact data
 
 module.exports = router;
