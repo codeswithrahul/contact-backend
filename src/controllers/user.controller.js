d43aa06969e49d4547e2dee5b194675d3a9f3f18
rahulcendrol <rahulchouhan.cendrol@gmail.com>
2026-01-05 09:52:32 +0530

    fix : monogdb issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,42 +1,42 @@
-// user.controller.js
-const { createUserService } = require("../services/user.service");
-const { fetchUsers } = require("../services/user.service");
+const { fetchUsers, createUserService } = require("../services/user.service");
 
-exports.getUsers = (req, res) => {
-  const users = fetchUsers();
-  res.json({
-    success: true,
-    data: users
-  });
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await fetchUsers();
+    res.json({
+      success: true,
+      data: users
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
 };
 
-
 exports.createUser = async (req, res) => {
-    try {
-      const { name, email } = req.body;
-  
-      if (!name || !email) {
-        return res.status(400).json({
-          success: false,
-          message: "Name and Email are required"
-        });
-      }
-  
-      const user = await createUserService({ name, email });
-  
-      res.status(201).json({
-        success: true,
-        message: "User created successfully",
-        data: user
-      });
-    } catch (error) {
-      console.error("Create user error:", error);
-  
-      res.status(500).json({
+  try {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).json({
         success: false,
-        message: "Internal Server Error",
-        error: error.message
+        message: "Name and Email are required"
       });
     }
-  };
-  
+
+    const user = await createUserService({ name, email });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: user
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
